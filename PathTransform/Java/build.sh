cf2bf3265b0ed01f78893a5426f77e4beefbea89
Wolfgang <wolfgang@wwendnagel-lk1.vhf.intern>
2020-07-31 09:56:23 +0200

    apply java coding conventions

--- PathTransform/Java/build.sh
+++ PathTransform/Java/build.sh
@@ -1,9 +1,9 @@
 #!/bin/bash -v
 
-DIR=./de/vhf/pathtransform
-mkdir -p $DIR
-rm $DIR/*
-javac -h . PathTransform.java
-mv *.class $DIR
-jar cf PathTransform.jar ./de
-g++ -I"$JAVA_HOME/include" -I"$JAVA_HOME/include/linux" -I"../../Common" -I.. -DNO_OPENCV -shared -o libPathTransform.so PathTransform.cpp ../../Common/*.cpp -fPIC
+JAVA_HOME="$(readlink -f $(dirname $(readlink -f $(which javac)))/..)"
+DIR=bin
+mkdir -p "${DIR}"
+rm -Rf "${DIR}"/*
+javac -h . src/de/vhf/pathtransform/PathTransform.java -d "${DIR}"
+jar cf PathTransform.jar -C "${DIR}" de
+g++ -I"${JAVA_HOME}/include" -I"${JAVA_HOME}/include/linux" -I"../../Common" -I.. -DNO_OPENCV -shared -o libPathTransform.so PathTransform.cpp ../../Common/*.cpp -fPIC
---
+++ PathTransform/Java/src/de/vhf/pathtransform/PathTransform.java
@@ -0,0 +1,136 @@
+package de.vhf.pathtransform;
+
+public class PathTransform {
+	static {
+		// Load native library PathTransform.dll (Windows) or libPathTransform.so (Unices)
+		System.loadLibrary("PathTransform");
+	}
+
+	public static class Path {
+		public static class Point {
+			public long x;
+			public long y;
+			public long z;
+
+			public Point(long x, long y, long z) {
+				this.x = x;
+				this.y = y;
+				this.z = z;
+			}
+		}
+
+		private long ptr = 0;
+
+		private native long createPath(int reservedSpace);
+
+		private native void deletePath(long pathPtr);
+
+		private native void pushBack(long pathPtr, long x, long y, long z);
+
+		private native long getPointPtr(long pathPtr, int index);
+
+		private native long getPointX(long pointPtr);
+
+		private native long getPointY(long pointPtr);
+
+		private native long getPointZ(long pointPtr);
+
+		private native int getPointCount(long pathPtr);
+
+		public Path(int reservedSpace) {
+			ptr = createPath(reservedSpace);
+		}
+
+		public Path(long pointer) {
+			this.ptr = pointer;
+		}
+
+		public void dispose() {
+			checkPointer(ptr);
+			deletePath(ptr);
+			ptr = 0;
+		}
+
+		public void pushBack(Point p) {
+			checkPointer(ptr);
+			pushBack(ptr, p.x, p.y, p.z);
+		}
+
+		public Point getPoint(int index) {
+			long pptr;
+
+			checkPointer(ptr);
+			pptr = getPointPtr(ptr, index);
+			return new Point(getPointX(pptr), getPointY(pptr), getPointZ(pptr));
+		}
+
+		public int getPointCount() {
+			checkPointer(ptr);
+			return getPointCount(ptr);
+		}
+
+		public void print() {
+			int pcnt = getPointCount();
+			for (int i = 0; i < pcnt; i++) {
+				Point p = getPoint(i);
+				System.out.println("x: " + p.x + " y: " + p.y + " z: " + p.z);
+			}
+		}
+	}
+
+	private long ptr = 0;
+
+	private native long createPathTransform(String representation);
+
+	private native void printPathTransform(long ptr);
+
+	private native void deletePathTransform(long ptr);
+
+	private native void transformInPlace(long ptr, long pathPtr);
+
+	private native long transform(long ptr, long pathPtr);
+
+	private static void checkPointer(long p) {
+		if (p == 0) {
+			throw new IllegalStateException("Internal Native Null Pointer detected");
+		}
+	}
+
+	public PathTransform(String representation) {
+		ptr = createPathTransform(representation);
+		if (ptr == 0) {
+			throw new IllegalArgumentException("Invalid String Representation of Transformation");
+		}
+	}
+
+	public void print() {
+		checkPointer(ptr);
+		printPathTransform(ptr);
+	}
+
+	public void dispose() {
+		checkPointer(ptr);
+		deletePathTransform(ptr);
+		ptr = 0;
+	}
+
+	/** transforms a path without subdivisions by modifying its coordinates directly */
+	public void transformInPlace(Path path) {
+		checkPointer(ptr);
+		checkPointer(path.ptr);
+		transformInPlace(ptr, path.ptr);
+	}
+
+	/**
+	 * creates a new path with the transformed input path as content. This also enables subdivisions if applicable. (=>
+	 * The resulting path may have more points than the input path.)
+	 */
+	public Path transform(Path path) {
+		long pathPtr;
+
+		checkPointer(ptr);
+		checkPointer(path.ptr);
+		pathPtr = transform(ptr, path.ptr);
+		return new Path(pathPtr);
+	}
+}
